604a31c8c382e934eae51ba690a5d34470de0a91
Eda <edaalbayrak86@gmail.com>
2023-01-03 16:49:27 +0300

    byLink

--- src/test/java/day03_locators/C04_ByLinkText.java
+++ src/test/java/day03_locators/C04_ByLinkText.java
@@ -9,36 +9,38 @@ import java.time.Duration;
 
 public class C04_ByLinkText {
     public static void main(String[] args) throws InterruptedException {
+
         // amazon anasayfaya gidin
         // Gift Cards linkini tiklayin
         // Gift Cards sayfasina gittigini test edin
 
-        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
 
         driver.get("https://www.amazon.com");
+        Thread.sleep(4000);
 
         // WebElement giftCardLinki= driver.findElement(By.linkText("Gift Cards"));
 
-        Thread.sleep(4000);
-        WebElement giftCardLinki=driver.findElement(By.partialLinkText("Gift"));
-        giftCardLinki.click();
+        //bazen tüm link text'i yazmasak da locate edebiliriz
+        //yalnızca link'in bir part'ını yazarak locate edebiliriz
+        WebElement giftCardLink=driver.findElement(By.partialLinkText("Gift"));
+        giftCardLink.click();
 
         // istenen sayfaya gittigini test etmek icin
-        // titkle'in Gift Card icerdigini test edebiliriz
+        // title'in Gift Card icerdigini test edebiliriz
 
-        String expectedKelime="Gift Cards";
-        String actualTitle=driver.getTitle();
+        String expectedWord="Gift Cards";
+        String actualTitle=driver.getTitle(); //actualTitle her zaman driver'ın aldığı değerdir
 
-        if (actualTitle.contains(expectedKelime)){
-            System.out.println("Gift Cards testi PASSED");
+        //testi yapalım
+        if (actualTitle.contains(expectedWord)){
+            System.out.println("Gift Cards test PASSED");
         }else {
-            System.out.println("Gift Card testi FAILED");
+            System.out.println("Gift Card test FAILED");
         }
-
         driver.close();
-
     }
 }
